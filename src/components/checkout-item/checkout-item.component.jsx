48be3fbae0bcccb94775a4c469b6b6d686dbfd19
oiva-terho <oiva.terho@gmail.com>
2022-09-25 11:30:46 +0300

    on wheel amoutn changer
    known bug:
    - scroll is not disabled

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,6 +11,11 @@ export const CheckoutItem = ({cartItem}) => {
   const increaseItemHandler = () => addItemToCart(cartItem);
   const decreaseItemHandler = () => removeItemFromCart(cartItem);
   const removeItemHandler = () => removeItemFromCart(cartItem, true);
+  const mosueChangeItemHandler = (e) => {
+    const change = e.deltaY / 100;
+    change > 0 ? addItemToCart(cartItem) : removeItemFromCart(cartItem);
+  
+  };
 
   return (
     <div className="checkout-item">
@@ -18,11 +23,11 @@ export const CheckoutItem = ({cartItem}) => {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className='checkout-item__name'></span>
-      <span className='checkout-item__quantity'>
+      <div className='checkout-item__quantity' onWheel={mosueChangeItemHandler}>
         <div className='checkout-item__quantity-arrow' onClick={decreaseItemHandler}>&#10094;</div>
         <span className='checkout-item__quantity-value'>{quantity}</span>
         <div className='checkout-item__quantity-arrow' onClick={increaseItemHandler}>&#10095;</div>
-      </span>
+      </div>
       <span className='checkout-item__price'>{price * quantity}</span>
       <div className='checkout-item__remove-btn'onClick={removeItemHandler}>&#10005;</div>
     </div>
