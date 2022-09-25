511e5e8697da0f874be4a0a1b8c378313a7d0bca
oiva-terho <oiva.terho@gmail.com>
2022-09-25 16:36:26 +0300

    shop data fetching from firebase db

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,25 @@
-import { useContext } from 'react';
+import { useContext, Fragment } from 'react';
 
-import { ProductsContext } from '../../contexts/products.context';
+import { CategoriesContext } from '../../contexts/categories.context';
 import { ProductCard } from '../../components/product-card/product-card.component';
 import './shop.styles.scss';
 
 export const Shop = () => {
-  const { products } = useContext(ProductsContext);
-  
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log(categoriesMap);
   return (
-    <div className='shop'>
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
+    <>
+      {
+      Object.keys(categoriesMap).map(title => (
+        <Fragment key={title}>
+          <h2>{title}</h2>
+          <div className='shop'>
+            {categoriesMap[title].map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Fragment>
       ))}
-    </div>
+    </>
   );
 };
