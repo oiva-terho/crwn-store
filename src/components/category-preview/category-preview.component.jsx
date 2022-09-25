ffc652a758a50ef6c7da1ef6025f84e921674fee
oiva-terho <oiva.terho@gmail.com>
2022-09-25 20:21:54 +0300

    navigate by Link

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,22 +1,16 @@
 import { ProductCard } from '../product-card/product-card.component';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './category-preview.styles.scss';
 
 export const CategoryPreview = ({title, products}) => {
-  const navigate = useNavigate();
-
-  const goToCategoryHandler = () => {
-    navigate(`/shop/${title}`);
-  };
 
   return (
     <div className='category-preview'>
       <h2>
-        <span 
-          className='category-preview__title' 
-          onClick={goToCategoryHandler}
-        >{title.toUpperCase()}</span>
+        <Link className='category-preview__title' to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className='category-preview__preview'>
         {
