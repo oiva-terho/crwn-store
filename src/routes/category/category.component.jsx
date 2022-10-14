e3213813b6fca1bf7dc1dfb70b47c0134b3200d7
oiva-terho <oiva.terho@gmail.com>
2022-10-14 18:09:23 +0300

    redux thunk
    - mooved async behavior of categories fetch to category reducer
    - added spinner for loading stage

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,13 +3,18 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { ProductCard } from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { Spinner } from '../../components/spinner/spinner.component';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading
+} from '../../store/categories/category.selector';
 
 import { Container, Title } from './category.styles';
 
 export const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,12 +24,16 @@ export const Category = () => {
   return (
     <>
       <Title>{category.toUpperCase()}</Title>
-      <Container>
-        {products &&
-          products.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </Container>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Container>
+          {products &&
+            products.map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </Container>
+      )}
     </>
   );
 };
